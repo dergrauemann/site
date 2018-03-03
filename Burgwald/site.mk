483ba9fd8b7c243fde25981c3a1c360444364726
FreifunkFKB <info@freifunk-fkb.de>
2018-03-03 18:10:32 +0100

    Addes Burgwald
    site.conf for Burgwald

---
+++ Burgwald/site.mk
@@ -0,0 +1,40 @@
+GLUON_SITE_PACKAGES := \
+        gluon-mesh-batman-adv-15 \
+        gluon-alfred \
+        gluon-respondd \
+        gluon-autoupdater \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-contact-info \
+        gluon-config-mode-core \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-hostname \
+        gluon-config-mode-tunneldigger \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-luci-theme \
+        gluon-luci-admin \
+        gluon-luci-autoupdater \
+        gluon-luci-wifi-config \
+        gluon-luci-portconfig \
+        gluon-luci-private-wifi \
+        gluon-next-node \
+        gluon-mesh-vpn-tunneldigger \
+        gluon-radvd \
+        gluon-setup-mode \
+        gluon-status-page \
+        gluon-migrate-vpn \
+        gluon-tunneldigger-watchdog \
+        ffho-wifi-blackout-workaround \
+        ffho-autoupdater-wifi-fallback \
+		ff-wtbg-autoreboot \
+        iwinfo \
+        iptables \
+        haveged
+
+
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?=en de
