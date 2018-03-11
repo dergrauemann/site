f18186f44ebbc810005ffc09d9611567992b20d5
FreifunkFKB <info@freifunk-fkb.de>
2018-03-11 15:21:34 +0100

    Changed Layout site.mk

--- Korbach/site.mk
+++ Korbach/site.mk
@@ -1,7 +1,7 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-respondd \
-    gluon-autoupdater \
+	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
