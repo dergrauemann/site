6ca1c552afab5289d1eb07b971f8afbead04e4f2
FreifunkFKB <mirsad@freifunk-fkb.de>
2022-11-12 22:30:55 +0100

    Update site.mk

--- Nordhessen/site.mk
+++ Nordhessen/site.mk
@@ -24,6 +24,7 @@ GLUON_REGION := eu
 
 GLUON_FEATURES := \
         autoupdater \
+	client-bridge \
 	ebtables-filter-multicast \
 	ebtables-filter-ra-dhcp \
 	ebtables-source-filter \
@@ -37,7 +38,6 @@ GLUON_FEATURES := \
 	web-private-wifi \
 	config-mode-geo-location-osm \
 	config-mode-domain-select \
-	client-bridge \
 	web-admin \
 	web-network \
         web-wizard
